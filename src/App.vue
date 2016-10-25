5708aef65c148d8eadbee3c9fee7fbcbaaa67183
Lorey <lorey@Leroys-MacBook-Pro.local>
2016-10-25 17:05:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="app">
+        <card v-for="(color, value) in values" :value="value" :color="color"></card>
+    </div>
+</template>
+
+<style>
+    body {
+        background-color: #E0E0E0;
+    }
+
+    #app {
+        text-align: center;
+    }
+</style>
+
+<script>
+    import Card from "./components/Card.vue";
+
+    export default {
+        data() {
+            return {
+                values: {
+                    "0": "#82b437",
+                    "1/2": "#1ba21f",
+                    "1": "#148e9c",
+                    "2": "#193cab",
+                    "3": "#252896",
+                    "5": "#422795",
+                    "8": "#c31077",
+                    "13": "#db0e15",
+                    "20": "#d97d09",
+                    "40": "#d3a708",
+                    "100": "#cfc108",
+                    "?":"#c7cd07",
+                    "BEER": "#c1c3af",
+                    "ELMO": "#ff1515"
+                }
+            };
+        },
+
+        components: {
+            Card
+        }
+    }
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="card">
+        <span class="card__background" :style="style">
+            <span class="card__value">
+                <template v-if="value == 'ELMO'">
+                    <img src="/assets/elmo.png">
+                </template>
+                <template v-else>
+                    {{ value }}
+                </template>
+            </span>
+        </span>
+    </div>
+</template>
+
+<style lang="sass">
+    .card {
+        display: inline-block;
+        margin: 15px;
+        box-sizing: border-box;
+        position: relative;
+        width: 11%;
+        border-radius: 15px;
+        background-color: white;
+        padding: 10px;
+        transition: all .2s;
+        user-select: none;
+    }
+
+    .card::before {
+        display: inline-block;
+        content: '';
+        padding-top: 160%;
+    }
+
+    .card:hover {
+        transform: scale(1.1, 1.1);
+    }
+
+    .card__background {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        bottom: 10px;
+        left: 10px;
+        border-radius: 15px;
+    }
+
+    .card__value {
+        display: block;
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-size: 72px;
+
+        img {
+            width: 80%;
+        }
+    }
+</style>
+
+<script>
+    var me = {
+        name: "Lorey",
+        occupation: "Life",
+        interests: ["Creating"]
+    };
+
+    export default {
+        props: {
+            'value': {
+                type: String,
+                default: ""
+            },
+            'color': {
+                type: String,
+                default: ""
+            }
+        },
+
+        mounted() {
+            console.log(this.color);
+        },
+
+        computed: {
+            style() {
+                return {
+                    'background-color': this.color
+                }
+            }
+        }
+    }
+</script>
